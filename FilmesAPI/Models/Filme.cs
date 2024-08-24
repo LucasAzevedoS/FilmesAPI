45555ea5eb52c8bf66419376b5bb971a7fd37455
Lucas Azevedo de Souza <lucas.a.souza@hotmail.com>
2024-08-24 11:49:27 -0300

    Add project files.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required]
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O titulo do filme ehh obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O titulo do genero ehh obrigatorio")]
+    [MaxLength(50, ErrorMessage ="O tamanho do genero nao pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duracao deve ter no minimo 70 e no maximo 600 minutos")]
+    public int Duracao { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "O nome do diretor nao pode exceder 50 caracteres")]
+    public string Diretor { get; set; }
+}
